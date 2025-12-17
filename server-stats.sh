603e8bc9f1877ae171991e1ca3acdc7f826d9d49
Jakub Rosak <kubarosak@o2.pl>
2025-12-17 15:31:18 +0100

    Add server-stats script

---
+++ server-stats.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "==========SERVER STATS==========="
+hostnamectl | awk '/Operating System/ {print("OS Version: ",$3,$4)}'
+uptime
+echo ""
+
+echo "CPU usage: $[100-$(vmstat 1 2|tail -1|awk '{print $15}')]%"
+free | awk '/Mem:/ {printf("Memory used: %.2f%%\n", $3/$2*100)}'
+echo "Disk memory used: $(df -h /|tail -1|awk '{print $5}')"
+
+echo "Top 5 processes by CPU usage:"
+ps -eo pid,cmd,%cpu --sort=-%cpu | head -n 6
+echo "Top 5 processes by memory usage:"
+ps -eo pid,cmd,%mem --sort=-%mem | head -n 6
